1414063b7cf09b4c2d0197cd3f4c004de2ce2957
kgrosul <34602954+kgrosul@users.noreply.github.com>
2018-05-12 18:25:18 +0300

    add wordcloud

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import os
 
 
 user_request = defaultdict(lambda: None)
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot('582824205:AAGaNsND2bCU6XtJ_x9VQjpzd0dNfqq3yhA')
 
 
 @bot.message_handler(commands=['new_docs'])
@@ -35,32 +35,37 @@ def reply_to_words(message):
 
 @bot.message_handler(commands=['doc'])
 def reply_to_doc(message):
-    bot.send_message(message.chat.id, 'Текст какого документа надо вывести?')
+    bot.send_message(message.chat.id,
+                     'Текст какого документа надо вывести?')
     user_request[message.chat.id] = 'doc'
 
 
 @bot.message_handler(commands=['describe_doc'])
 def reply_to_describe_doc(message):
-    bot.send_message(message.chat.id, 'Статистику по какому документу надо вывести?')
+    bot.send_message(message.chat.id,
+                     'Статистику по какому документу надо вывести?')
     user_request[message.chat.id] = 'describe_doc'
 
 
 @bot.message_handler(commands=['describe_topic'])
 def reply_to_describe_topic(message):
-    bot.send_message(message.chat.id, 'Статистику по какой теме надо вывести?')
+    bot.send_message(message.chat.id,
+                     'Статистику по какой теме надо вывести?')
     user_request[message.chat.id] = 'describe_topic'
 
 
-@bot.message_handler(commands=['tag'])
+@bot.message_handler(commands=['help', 'start'])
 def reply_to_describe_topic(message):
-    bot.send_message(message.chat.id, 'Документы с какими тегами надо вывести?')
-    user_request[message.chat.id] = 'tag'
-
-
-@bot.message_handler(commands=['fun'])
-def reply_to_describe_topic(message):
-    bot.send_message(message.chat.id, 'Какая тема?)')
-    user_request[message.chat.id] = 'fun'
+    text = "Привет! Вот что умеет бот:\n" + \
+            "/help - список возможностей\n" + \
+            "/new_docs - самые свежие новости\n" + \
+            "/new_topics - самые свежие темы\n" + \
+            "/topic - описание темы\n" + \
+            "/words - 5 слов лучше всего описывающих тему\n" + \
+            "/doc - текст документа\n" + \
+            "/describe_doc - статистика по документу\n" + \
+            "/describe_topic - статистика по теме"
+    bot.send_message(message.chat.id, text)
 
 
 @bot.message_handler(content_types=['text'])
@@ -72,9 +77,9 @@ def reply_to_text(message):
                 response_text = str(i+1)+". " + documents[i].title + \
                     "\n\n" + documents[i].url
                 bot.send_message(message.chat.id, response_text)
-            del user_request[message.chat.id]
         else:
-            bot.send_message(message.chat.id, "Введите корректное число")
+            bot.send_message(message.chat.id,
+                             "Введите корректное число")
 
     elif user_request[message.chat.id] == 'new_topics':
         if message.text.isdigit():
@@ -86,10 +91,9 @@ def reply_to_text(message):
                     "\n\n" + topics[i].url
                 bot.send_message(message.chat.id, response_text)
 
-            del user_request[message.chat.id]
-
         else:
-            bot.send_message(message.chat.id, "Введите корректное число")
+            bot.send_message(message.chat.id,
+                             "Введите корректное число")
 
     elif user_request[message.chat.id] == 'topic':
         description = make_request.get_topic_description(message.text)
@@ -104,10 +108,9 @@ def reply_to_text(message):
                     "\n\n" + documents[i].url
                 bot.send_message(message.chat.id, response_text)
 
-            del user_request[message.chat.id]
-
         else:
-            bot.send_message(message.chat.id, "Введите корректное название темы")
+            bot.send_message(message.chat.id,
+                             "Введите корректное название темы")
 
     elif user_request[message.chat.id] == 'words':
         tags = make_request.get_best_words(message.text, 5)
@@ -118,7 +121,8 @@ def reply_to_text(message):
             del user_request[message.chat.id]
 
         else:
-            bot.send_message(message.chat.id, "Введите корректное название темы")
+            bot.send_message(message.chat.id,
+                             "Введите корректное название темы")
 
     elif user_request[message.chat.id] == 'doc':
         text = make_request.get_document_text(message.text)
@@ -129,11 +133,13 @@ def reply_to_text(message):
             del user_request[message.chat.id]
 
         else:
-            bot.send_message(message.chat.id, "Введите корректное название документа")
+            bot.send_message(message.chat.id,
+                             "Введите корректное название документа")
 
     elif user_request[message.chat.id] == 'describe_doc':
         plot1_file_name = 'plot1'+str(message.chat.id)+'.png'
         plot2_file_name = 'plot2'+str(message.chat.id)+'.png'
+        word_cloud_file_name = 'wcloud'+str(message.chat.id)+'.png'
         result = make_request.make_distribution_plot(message.text,
                                                      'document',
                                                      plot1_file_name,
@@ -144,36 +150,56 @@ def reply_to_text(message):
                 bot.send_photo(message.chat.id, plot1)
             with open(plot2_file_name, 'rb') as plot2:
                 bot.send_photo(message.chat.id, plot2)
+            make_request.document_word_cloud(message.text,
+                                             word_cloud_file_name)
+            with open(word_cloud_file_name, 'rb') as word_cloud:
+                bot.send_photo(message.chat.id, word_cloud)
             os.remove(plot1_file_name)
             os.remove(plot2_file_name)
+            os.remove(word_cloud_file_name)
         else:
-            bot.send_message(message.chat.id, "Введите корректное название документа")
+            bot.send_message(message.chat.id,
+                             "Введите корректное название документа")
 
     elif user_request[message.chat.id] == 'describe_topic':
         plot1_file_name = 'plot1'+str(message.chat.id)+'.png'
         plot2_file_name = 'plot2'+str(message.chat.id)+'.png'
+        word_cloud_file_name = 'wcloud'+str(message.chat.id)+'.png'
         result = make_request.make_distribution_plot(message.text,
                                                      'topic',
                                                      plot1_file_name,
                                                      plot2_file_name
                                                      )
         if result:
-            bot.send_message(message.chat.id, "Средняя длина документа: " +
-                             str(make_request.get_avg_document_len(message.text)))
-            bot.send_message(message.chat.id, "Всего документов: " +
-                             str(make_request.get_documents_number(message.text)))
+            bot.send_message(message.chat.id,
+                             "Средняя длина документа: " +
+                             str(make_request.
+                                 get_avg_document_len(message.text)))
+
+            bot.send_message(message.chat.id,
+                             "Всего документов: " +
+                             str(make_request.
+                                 get_documents_number(message.text)))
+
             with open(plot1_file_name, 'rb') as plot1:
                 bot.send_photo(message.chat.id, plot1)
             with open(plot2_file_name, 'rb') as plot2:
                 bot.send_photo(message.chat.id, plot2)
+            make_request.topic_word_cloud(message.text,
+                                          word_cloud_file_name)
+            with open(word_cloud_file_name, 'rb') as word_cloud:
+                bot.send_photo(message.chat.id, word_cloud)
             os.remove(plot1_file_name)
             os.remove(plot2_file_name)
+            os.remove(word_cloud_file_name)
         else:
-            bot.send_message(message.chat.id, "Введите корректное название Темы")
+            bot.send_message(message.chat.id,
+                             "Введите корректное название темы")
+
 
 if __name__ == '__main__':
     while True:
         try:
             bot.polling(none_stop=True)
         except requests.exceptions.ReadTimeout:
-            print("А вот сейчас бот упал из-за pollong")
+            print("А вот сейчас бот упал из-за polling")
