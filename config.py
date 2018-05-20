12e8c338864ff28ab34455b4d99720ee99fab10c
kgrosul <34602954+kgrosul@users.noreply.github.com>
2018-05-21 02:11:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-MAX_OCCURRENCE = 15
-MIN_OCCURRENCE = 2
+MAX_OCCURRENCE = 15 # Макс. число вхождений в текст для слова 
+MIN_OCCURRENCE = 2 # Мин. число вхождений в текст для слова 
 
-PICTURE_WIDTH = 960
-PICTURE_HEIGHT = 960
+PICTURE_WIDTH = 960 # Ширина картинки, отправляемой пользователю
+PICTURE_HEIGHT = 960 # Высота картинки, отправляемой пользователю
 
-CLOUD_MAX_WORDS = 200
+CLOUD_MAX_WORDS = 200 # Максимальное количество слов в WordCloud
 
-REDIRECTS = 100
+REDIRECTS = 100 # Максимальное количество перенаправлений при парсинге
 
-DEFAULT_NEW_DOCS = 5
-DEFAULT_NEW_TOPICS = 5
+DEFAULT_NEW_DOCS = 5 # Число свежих новостей, которое выводится по умолчанию
+DEFAULT_NEW_TOPICS = 5 # Число свежих тем, которое выводится по умолчанию
 
-BEST_WORDS_NUM = 5
+BEST_WORDS_NUM = 5 # Число слов для описания темы, которое выводится по умолчанию
